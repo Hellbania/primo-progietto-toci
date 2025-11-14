651dcb6da53ecd85a0187811993f146a7063b55b
toci.anxhelo <toci.anxhelo@iisgalvanimi.edu.it>
2025-11-14 09:25:22 +0000

    Complete exercise: add nuovo.html, nuovostile.css, novoscript.js and translate to English

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
 console.log("hello");
 
 (function(){
   const path = location.pathname.split("/").pop();
   if (path === "me.html") {
-    console.log("ciao");
-    alert("ciao");
+    console.log("hello");
+    alert("hello");
   }
 })();
